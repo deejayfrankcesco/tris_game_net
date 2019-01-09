90677a08246869b99bb86a680348de40c6d28f12
Francesco Martina <deejayfrankcesco@gmail.com>
2019-01-09 17:31:45 +0100

    Update tictactoe_client.c

--- tictactoe_client.c
+++ tictactoe_client.c
@@ -24,7 +24,7 @@
 
 ///////// Macro definitions
 //#define tcp_port		5000											//TCP port
-//#define IP 				"127.0.0.1"
+//#define IP 			"127.0.0.1"
 
 #define RX_BUFFER_SIZE	40												//size of TCP rx buffer
 #define TX_BUFFER_SIZE	40												//size of TCP tx buffer
@@ -32,13 +32,13 @@
 //////////////////////// Var & Types //////////////////////
 
 ///////// Global variables
-int port;																//TCP port
-char host[40];															//host IP or name
-int sock;																//connection socket
-char rx_buffer[RX_BUFFER_SIZE];											//define rx_buffer
-char tx_buffer[TX_BUFFER_SIZE];											//define tx_buffer
-int ret;																//General Purpose return variable
-enum {MENU,GAME,DISCONNECTED} status;									//0 in menu, 1 if game is running, 2 if server has disconnected
+int port;														//TCP port
+char host[40];														//host IP or name
+int sock;														//connection socket
+char rx_buffer[RX_BUFFER_SIZE];												//define rx_buffer
+char tx_buffer[TX_BUFFER_SIZE];												//define tx_buffer
+int ret;														//General Purpose return variable
+enum {MENU,GAME,DISCONNECTED} status;											//0 in menu, 1 if game is running, 2 if server has disconnected
 
 //////////////////////// Functions ////////////////////////
 void print_command_list(){
@@ -58,9 +58,9 @@ void hashtag(){
 //read line from terminal and delete '\n' at the end
 void reader(){
 	char* p;
-	fgets(tx_buffer, TX_BUFFER_SIZE, stdin);							//read command
+	fgets(tx_buffer, TX_BUFFER_SIZE, stdin);									//read command
 	p=strchr(tx_buffer,'\n');
-	*p='\0';															//subtitute '\n' with '\0'
+	*p='\0';													//subtitute '\n' with '\0'
 }
 
 //print reply message from server
@@ -68,19 +68,19 @@ void receiver(){
 	do{
 		ret = (int)recv(sock,rx_buffer, RX_BUFFER_SIZE, MSG_WAITALL);
 		//command by server (all mutual exclusive)
-		if(!strcmp(rx_buffer,"#")) status = GAME; else					//set client status to game
-		if(!strcmp(rx_buffer,">")) status = MENU; else					//set client status to normal menu
-		if(!strcmp(rx_buffer,"@@")) {printf("\n"); return;} else		//message has ended
-		if(!strcmp(rx_buffer,"@ack")) send(sock, rx_buffer, RX_BUFFER_SIZE, 0);	//resend @ack
+		if(!strcmp(rx_buffer,"#")) status = GAME; else								//set client status to game
+		if(!strcmp(rx_buffer,">")) status = MENU; else								//set client status to normal menu
+		if(!strcmp(rx_buffer,"@@")) {printf("\n"); return;} else						//message has ended
+		if(!strcmp(rx_buffer,"@ack")) send(sock, rx_buffer, RX_BUFFER_SIZE, 0);					//resend @ack
 
 		else
-		if(ret>0)printf("%s", rx_buffer);								//append message
+		if(ret>0)printf("%s", rx_buffer);									//append message
 
 	}while(ret>0);
 
 	status = DISCONNECTED;
 	printf("\n\n");
-	return;																//server has disconnected
+	return;														//server has disconnected
 }
 
 
@@ -109,7 +109,7 @@ void init_command(){
     strcpy(command_list[9],"!hit 9");
 }
 
-char interpreter(char* command){                               			//command interpreter
+char interpreter(char* command){                               								//command interpreter
     if(!strcmp(command,command_list[0])) return 0;
     if(!strcmp(command,command_list[1])) return 1;
     if(!strcmp(command,command_list[2])) return 2;
@@ -126,7 +126,7 @@ char interpreter(char* command){                               			//command inte
     if(!strcmp(command,command_list[13])) return 13;
     if(!strcmp(command,command_list[14])) return 14;
     if(!strcmp(command,command_list[15])) return 15;
-    return -1;                                                  		//return -1 if command is unknown
+    return -1;                                                  							//return -1 if command is unknown
 }
 ///////
 
@@ -149,17 +149,17 @@ int main(int argc, char const *argv[])
 	struct sockaddr_in server_addr; 
 
 	printf("Opening TCP socket\n");
-	sock = socket(PF_INET, SOCK_STREAM, 0);								//create TCP socket
-	if(sock == -1){														//checks for errors
+	sock = socket(PF_INET, SOCK_STREAM, 0);										//create TCP socket
+	if(sock == -1){													//checks for errors
 		printf("ERROR <-- creating socket\n");
 		exit(-1);
 	}	
 
-	memset(&server_addr, 0, sizeof(struct sockaddr_in));				//reset server_addr structure
+	memset(&server_addr, 0, sizeof(struct sockaddr_in));								//reset server_addr structure
 
 	//structure configurations
-	server_addr.sin_family = AF_INET;									//use IPv4 address	
-	server_addr.sin_port = htons(port);									//main listen port
+	server_addr.sin_family = AF_INET;										//use IPv4 address	
+	server_addr.sin_port = htons(port);										//main listen port
 	ret = inet_pton(AF_INET, host, &server_addr.sin_addr);
 
 	ret = connect(sock, (struct sockaddr*) &server_addr, sizeof(server_addr));	//connect server
@@ -170,10 +170,10 @@ int main(int argc, char const *argv[])
 	printf("Connected!\n\n");
 
 
-	printf("Insert your username: \n");									//asking credentials
-	reader();															//read username
+	printf("Insert your username: \n");										//asking credentials
+	reader();													//read username
 
-	send(sock, tx_buffer, TX_BUFFER_SIZE, 0);							//send username
+	send(sock, tx_buffer, TX_BUFFER_SIZE, 0);									//send username
 	recv(sock, rx_buffer , RX_BUFFER_SIZE , MSG_WAITALL);
 	if(!strcmp(rx_buffer,"OK")){
 		printf("Connected with server and registered!\n");
@@ -189,32 +189,32 @@ int main(int argc, char const *argv[])
 	print_command_list();
 	while(1){
 		if(status == MENU){
-			prompt();													//write command insert '>'
-			reader();													//read command
+			prompt();											//write command insert '>'
+			reader();											//read command
 			switch (interpreter(tx_buffer)){
-				case 0:	print_command_list(); break;					//!help stamps command list
+				case 0:	print_command_list(); break;							//!help stamps command list
 
-				case 10:												//!who
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 10:										//!who
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 
-				case 11:												//!create
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 11:										//!create
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 
-				case 12:												//!join
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
-				reader();												//read command
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 12:										//!join
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
+				reader();										//read command
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 
-				case 14:												//!quit
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 14:										//!quit
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 
 				//hit # commands
@@ -228,44 +228,44 @@ int main(int argc, char const *argv[])
 				case 8: printf("This command works only while you are playing\n");	break;
 				case 9: printf("This command works only while you are playing\n");	break;
 				//
-				case 13: printf("This command works only while you are playing\n");	break;	//!disconnect
-				case 15: printf("This command works only while you are playing\n");	break;	//!show_map
-				case 16: printf("This command works only while you are playing\n");	break;	//!hit
+				case 13: printf("This command works only while you are playing\n");	break;		//!disconnect
+				case 15: printf("This command works only while you are playing\n");	break;		//!show_map
+				case 16: printf("This command works only while you are playing\n");	break;		//!hit
 				default: printf("Unknown command, retry\n"); break;
 			}
 
 		}else if(status == GAME){
-			hashtag();													//write command insert '>'
-			reader();													//read command
+			hashtag();											//write command insert '>'
+			reader();											//read command
 			switch (interpreter(tx_buffer)){
-				case 0:	print_command_list(); break;					//!help stamps command list
+				case 0:	print_command_list(); break;							//!help stamps command list
 				
-				case 1:case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9:		//hit cells
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 1:case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9:			//hit cells
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 				
-				case 13:												//!disconnect
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 13:										//!disconnect
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 
-				case 15:												//!show_map
-				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);				//send command
-				receiver();												//print text sent by server and check connection
+				case 15:										//!show_map
+				send(sock, tx_buffer, TX_BUFFER_SIZE, 0);						//send command
+				receiver();										//print text sent by server and check connection
 				break;
 				
 
-				case 10: printf("This command works only when you are not playing\n"); break;	//!who
-				case 11: printf("This command works only when you are not playing\n"); break;	//!create
-				case 12: printf("This command works only when you are not playing\n"); break;	//!join
-				case 14: printf("This command works only when you are not playing\n"); break;	//!quit
+				case 10: printf("This command works only when you are not playing\n"); break;		//!who
+				case 11: printf("This command works only when you are not playing\n"); break;		//!create
+				case 12: printf("This command works only when you are not playing\n"); break;		//!join
+				case 14: printf("This command works only when you are not playing\n"); break;		//!quit
 				default: printf("Unknown command, retry\n"); break;
 			}
 
 
-		}else{															//aka. status = DISCONNECTED
-			break;														//quitting
+		}else{													//aka. status = DISCONNECTED
+			break;												//quitting
 		}
 
 		}
